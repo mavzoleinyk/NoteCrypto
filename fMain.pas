bf37fde7a73868f5705e18c494b14f5b67bdf358
Vitalii <itstart.com.ua@gmail.com>
2022-09-25 13:32:43 +0300

    Add all files

---
+++ fMain.pas
@@ -0,0 +1,44 @@
+unit fMain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Menus;
+
+type
+  TForm1 = class(TForm)
+    FontDialog1: TFontDialog;
+    MainMenu1: TMainMenu;
+    Memo1: TMemo;
+    MenuItem1: TMenuItem;
+    MenuItem10: TMenuItem;
+    MenuItem11: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    MenuItem7: TMenuItem;
+    MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
+    OpenDialog1: TOpenDialog;
+    SaveDialog1: TSaveDialog;
+    Separator2: TMenuItem;
+    Separator1: TMenuItem;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
